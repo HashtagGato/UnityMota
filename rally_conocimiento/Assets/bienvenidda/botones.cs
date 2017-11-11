c981b5ff9547033c511757b83b6f5eabc9d03a5d
Adolfo <assassin_black@hotmail.com>
2017-11-10 20:24:12 -0600

    Inicio del rally

---
+++ rally_conocimiento/Assets/bienvenidda/botones.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botones : MonoBehaviour {
+    public void BotonJugar()
+    {
+#pragma warning disable CS0618 // Type or member is obsolete
+        //Application.LoadLevel("escenaCamara");
+#pragma warning restore CS0618 // Type or member is obsolete
+        //Dirigirse a sig_edificio
+    }
+
+    public void BotonReanudar()
+    {
+        //Application.LoadLevel("hol");
+    }
+
+    public void BotonSalir()
+    {
+
+    }
+}
+
+
---
+++ rally_conocimiento/Assets/login/login_registro.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class login_registro : MonoBehaviour {
+
+	public void Login()
+    {
+
+    }
+
+    public void Register()
+    {
+
+    }
+}
