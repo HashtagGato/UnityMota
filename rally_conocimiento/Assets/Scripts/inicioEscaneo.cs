9072922258dfe4021d9e628dab751b0193d44b4b
--replace-all <alex.belikov19@gmail.com>
2017-11-25 08:51:41 -0600

    Subo unos pequeños cambios que hice por si alguien trabajara con esto ahorita, tecnicamente ya esta la obtencion de la pregunta, solo falta borrar unas lineas y afinar detalles

--- rally_conocimiento/Assets/Scripts/inicioEscaneo.cs
+++ rally_conocimiento/Assets/Scripts/inicioEscaneo.cs
@@ -18,25 +18,6 @@ public class inicioEscaneo : MonoBehaviour{
 		Screen.orientation = ScreenOrientation.LandscapeLeft;
 
         StartCoroutine("startPregunta");
-
-        string nEdificio = "Z";//Remplazar por el index del edificio que regresa el web service.
-		but = GameObject.Find ("CanvasResp");
-		cbut = but.GetComponent<Canvas> ();
-		for(int i=0; i<nEdificios.Length;i++){
-			mImage = GameObject.Find ("ImageTarget" + nEdificios[i]);
-			if (!nEdificio.Equals (nEdificios [i]))
-				mImage.SetActive (false);
-		}
-		tPreg = GameObject.Find ("preguntaText"+nEdificio).GetComponent<Text> ();
-		tResp1 = GameObject.Find ("TextA").GetComponent<Text> ();
-		tResp2 = GameObject.Find ("TextB").GetComponent<Text> ();
-		tResp3 = GameObject.Find ("TextC").GetComponent<Text> ();
-		tResp4 = GameObject.Find ("TextD").GetComponent<Text> ();
-		tResp1.text = "Resp A";//Asignar lo consumido por el webService, en cada una
-		tResp2.text = "Resp B";
-		tResp3.text = "Resp C";
-		tResp4.text = "Resp D";
-		tPreg.text = "Hola mi nombre es haruko y esta es tu siguiente pregunta, ¿cuántos años se toma para viajar a la luna?";
 	}
 	
 	// Update is called once per frame
@@ -57,26 +38,68 @@ public class inicioEscaneo : MonoBehaviour{
 
         WWW www = new WWW(url);
         yield return www;
-        if(www.error == null)
+        if (www.error == null)
         {
-            string [] json = www.text.Split(':');
-            string pregunta = json[1].Split(',')[1];
+            System.Text.Encoding utf_8 = System.Text.Encoding.BigEndianUnicode;
+            string json = www.text; //json obtenido desde el WS
+                                    //byte[] utf8_bytes = System.Text.Encoding.UTF8.GetBytes(json.Replace("\"",""));
+                                    // byte[] utf8_bytes = System.Text.Encoding.BigEndianUnicode.GetBytes(json.Replace("\"", ""));
+                                    //string jsonUTF8 = System.Text.Encoding.BigEndianUnicode.GetString(utf8_bytes);//json codificado en UTF8
 
-            //cambio a utf8, esta es una prueba, la dejo para trabajar mas tarde --------------------------------------
-            System.Text.Encoding utf_8 = System.Text.Encoding.UTF8;
+            //string a = System.Text.Encoding.ASCII(json); 
 
-            string s_unicode = "\u00bfCual es el disco m\u00e1s vendido de la historia?";
-            byte[] utf8_bytes = System.Text.Encoding.UTF8.GetBytes(s_unicode);
+            string[] json_array = json.Split(':');
 
-            string dos = System.Text.Encoding.UTF8.GetString(utf8_bytes);
-            Debug.Log(dos); 
 
+            string pregunta = json_array[1].Split(',')[0];
+            string op1 = json_array[2].Split(',')[0];
+            string op2 = json_array[2].Split(',')[1];
+            string op3 = json_array[2].Split(',')[2];
+            string op4 = json_array[2].Split(',')[3];
+
+            string correcta = json_array[3].Split('}')[0];
+
+            string nEdificio = "Z";//Remplazar por el index del edificio que regresa el web service.
+            but = GameObject.Find("CanvasResp");
+            cbut = but.GetComponent<Canvas>();
+            for (int i = 0; i < nEdificios.Length; i++)
+            {
+                mImage = GameObject.Find("ImageTarget" + nEdificios[i]);
+                if (!nEdificio.Equals(nEdificios[i]))
+                    mImage.SetActive(false);
+            }
+            tPreg = GameObject.Find("preguntaText" + nEdificio).GetComponent<Text>();
+            tResp1 = GameObject.Find("TextA").GetComponent<Text>();
+            tResp2 = GameObject.Find("TextB").GetComponent<Text>();
+            tResp3 = GameObject.Find("TextC").GetComponent<Text>();
+            tResp4 = GameObject.Find("TextD").GetComponent<Text>();
+            tResp1.text = json_array[2].Split(',')[0];//Asignar lo consumido por el webService, en cada una
+            tResp2.text = json_array[2].Split(',')[1];
+            tResp3.text = json_array[2].Split(',')[2];
+            tResp4.text = json_array[2].Split(',')[3];
+            tPreg.text = json_array[1].Split(',')[0];
+
+            Debug.Log(json_array[2]);
+
+            
             //----------------------------------------------------------------------------------------------------------
 
             //Debug.Log(jsonUTF8);
         } else
         {
-            
+
+            //cambio a utf8, esta es una prueba, la dejo para trabajar mas tarde --------------------------------------
+            System.Text.Encoding utf_8 = System.Text.Encoding.BigEndianUnicode;
+
+            string s_unicode = "[{ \"pregunta\":\"\u00bfCual es el disco m\u00e1s vendido de la historia?\",\"respuesta\":\"Servicio de lavander\u00eda - Shakira,Thriller-Michael Jackson,Supernatural - Santana,Hotel California - Eagles \",\"respuesta_correcta\":\"1\"}]";
+            byte[] utf8_bytes = System.Text.Encoding.BigEndianUnicode.GetBytes(s_unicode.Replace("\"",""));
+
+            string dos = System.Text.Encoding.BigEndianUnicode.GetString(utf8_bytes);
+
+            string pregunta = dos.Split(':')[1].Split(',')[0];
+
+            Debug.Log(pregunta);
+
             Debug.Log(string.Concat(www.error, " aqui marca el error"));
         }
         
