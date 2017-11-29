a382d6b17dd53b89ce5b604e7e8de56e79f1f21b
Alejandra Manriquez Chavez <jade_06_16@hotmail.com>
2017-11-29 04:50:24 -0600

    Audio de pregunta

--- rally_conocimiento/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
+++ rally_conocimiento/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -23,8 +23,11 @@ namespace Vuforia
 		private Canvas cResps, cPreg, cSig;
 		private string sName;
 		private sig_edificio sigEdif;
-		GameObject gameObjectScript;
-
+		GameObject gameObjectScript, mov;
+		public  AudioClip a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21,a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, a32, a33, a34, a35, a36, a37, a38, a39, a40,a41, a42, a43, a44, a45, a46, a47, a48, a49, a50;
+		AudioClip[] audios;
+		public AudioSource fuente;
+		private inicioEscaneo iE;
         #endregion // PRIVATE_MEMBER_VARIABLES
 
 
@@ -33,6 +36,12 @@ namespace Vuforia
     
         void Start()
 		{
+			fuente = GetComponent<AudioSource> ();
+			AudioClip[] auxAudios = {a1, a2,a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21,a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, a32, a33, a34, a35, a36, a37, a38, a39, a40,a41, a42, a43, a44, a45, a46, a47, a48, a49, a50};
+			audios = auxAudios;
+
+			mov = GameObject.Find ("ScriptMov");
+			iE = mov.GetComponent<inicioEscaneo> ();
 			gameObjectScript = GameObject.Find("script");
 			sigEdif = gameObjectScript.GetComponent<sig_edificio>();
             mTrackableBehaviour = GetComponent<TrackableBehaviour>();
@@ -70,7 +79,11 @@ namespace Vuforia
 				if (mTrackableBehaviour.TrackableName.Equals (sigEdif.obtenerEd ())) {
 					cResps.enabled = true;
 					cPreg.enabled = true;
+					fuente.clip = audios [(iE.getPreguntaID ())-1];
+					Debug.Log ("El de ABAJO " + iE.getPreguntaID ());
+					fuente.Play ();
 				}
+
             }
             else
             {
@@ -78,6 +91,8 @@ namespace Vuforia
 				cResps.enabled = false;
 				cPreg.enabled = false;
 				cSig.enabled = false;
+				fuente.clip = audios [(iE.getPreguntaID ())-1];
+				fuente.Stop ();
             }
         }
 
